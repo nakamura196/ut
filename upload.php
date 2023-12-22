92391053382c7280922394caae64d361ae4b0f1c
Satoru Nakamura <na.kamura.1263@gmail.com>
2023-12-22 12:59:29 +0900

    Update upload.php

--- upload.php
+++ upload.php
@@ -7,7 +7,10 @@ header('Content-Type: application/json');
 // APIキーのチェック
 if ($_POST['apiKey'] !== $expectedApiKey) {
     http_response_code(403); // 不正なアクセス
-    echo json_encode(["error" => "Invalid API Key"]);
+    echo json_encode([
+        "error" => "Invalid API Key",
+        "receivedApiKey" => $_POST['apiKey'] // 受け取ったAPIキー
+    ]);
     exit;
 }
 
@@ -17,7 +20,10 @@ $path = $_POST["path"];
 // パスの安全性を確認
 if (!preg_match('/^[a-zA-Z0-9\/\._-あ-んア-ン一-龥]+$/', $path)) {
     http_response_code(400); // 不正なリクエスト
-    echo json_encode(["error" => "Invalid Path"]);
+    echo json_encode([
+        "error" => "Invalid Path",
+        "receivedPath" => $path // 受け取ったパス
+    ]);
     exit;
 }
 
@@ -30,20 +36,30 @@ if (!file_exists($dirname)) {
 $fullPath = $root . $path;
 if (file_exists($fullPath)) {
     http_response_code(409); // コンフリクト
-    echo json_encode(["error" => "File already exists."]);
+    echo json_encode([
+        "error" => "File already exists.",
+        "fullPath" => $fullPath // フルパス
+    ]);
     exit;
 }
 
 // ファイルアップロード処理
 if (isset($_FILES['media']) && is_uploaded_file($_FILES['media']['tmp_name'])) {
     if (move_uploaded_file($_FILES['media']['tmp_name'], $fullPath)) {
-        echo json_encode(["success" => true, "message" => "File uploaded successfully."]);
+        echo json_encode([
+            "success" => true,
+            "message" => "File uploaded successfully.",
+            "fullPath" => $fullPath // アップロードされたファイルのフルパス
+        ]);
     } else {
         http_response_code(500); // サーバーエラー
         echo json_encode(["error" => "Failed to upload file."]);
     }
 } else {
     http_response_code(400); // 不正なリクエスト
-    echo json_encode(["error" => "No file uploaded."]);
+    echo json_encode([
+        "error" => "No file uploaded.",
+        "receivedPath" => $path // 受け取ったパス
+    ]);
 }
 ?>

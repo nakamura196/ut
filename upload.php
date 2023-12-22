faf1d9db4c826587014bfe27121665cd717885d1
Satoru Nakamura <na.kamura.1263@gmail.com>
2023-12-22 12:34:18 +0900

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,9 +1,41 @@
 <?php
-  $root = "./"; //適宜変更してください。
-  $path = $_POST["path"];
-  $dirname = dirname($path);
-  if (!file_exists($dirname)) {
-      mkdir($dirname, 0755, true);
-  }
-  move_uploaded_file($_FILES['media']['tmp_name'], $root.$path);
+// APIキーの設定
+$expectedApiKey = 'YOUR_EXPECTED_API_KEY'; // ここに期待するAPIキーを設定してください。
+
+header('Content-Type: application/json');
+
+// APIキーのチェック
+if ($_POST['apiKey'] !== $expectedApiKey) {
+    http_response_code(403); // 不正なアクセス
+    echo json_encode(["error" => "Invalid API Key"]);
+    exit;
+}
+
+$root = "./"; // 適宜変更してください。
+$path = $_POST["path"];
+
+// パスの安全性を確認
+if (!preg_match('/^[a-zA-Z0-9\/\._-]+$/', $path)) {
+    http_response_code(400); // 不正なリクエスト
+    echo json_encode(["error" => "Invalid Path"]);
+    exit;
+}
+
+$dirname = dirname($path);
+if (!file_exists($dirname)) {
+    mkdir($dirname, 0755, true);
+}
+
+// ファイルアップロード処理
+if (isset($_FILES['media']) && is_uploaded_file($_FILES['media']['tmp_name'])) {
+    if (move_uploaded_file($_FILES['media']['tmp_name'], $root . $path)) {
+        echo json_encode(["success" => true, "message" => "File uploaded successfully."]);
+    } else {
+        http_response_code(500); // サーバーエラー
+        echo json_encode(["error" => "Failed to upload file."]);
+    }
+} else {
+    http_response_code(400); // 不正なリクエスト
+    echo json_encode(["error" => "No file uploaded."]);
+}
 ?>
